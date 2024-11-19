fc8d3f69b14d5fbaa13e1cc750e52636fd34c147
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-19 13:26:45 +0100

    Avoir historique avec le médecin (afficher dans la visite ses participation)

--- models/event_registration.py
+++ models/event_registration.py
@@ -4,6 +4,17 @@ class EventRegistration(models.Model):
     _inherit = 'event.registration'
     resrvedBy = fields.Many2one('hr.employee', string="Réservé par")
     partner_id = fields.Many2one("res.partner", string="Participant")
+    sponsored_by = fields.Many2many('res.partner', string="Parrainer par", compute="getAllSponredPartner")
     name = fields.Char(related="partner_id.name")
     email = fields.Char(related="partner_id.email")
     phone = fields.Char(related="partner_id.phone")
+
+    @api.depends('partner_id')
+    def getAllSponredPartner(self):
+        for rec in self:
+            partner_ids = self.env['res.partner'].sudo().search([
+                ('sponsorship', 'in', [rec.partner_id.id])
+            ])
+            if partner_ids: rec.sponsored_by = partner_ids.ids
+            else: rec.sponsored_by = []
+
