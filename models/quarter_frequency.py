6b18f5a1091284206d8d80ac1ccebc0676108903
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-19 22:55:57 +0100

    ajouter le type de fréquance

--- models/quarter_frequency.py
+++ models/quarter_frequency.py
@@ -4,8 +4,16 @@ class QuarterFrequency(models.Model):
     _name = "quarter.frequency"
     _inherit = ["mail.thread", "mail.activity.mixin"]
 
+    @api.model
+    def _get_type_freq(self):
+        selection = []
+        for emp in self.env["type.freq"].search([]):
+            selection += [("%s" % emp.id, "%s" % emp.description)]
+        return selection
 
     name = fields.Char("Trimestre")
     year = fields.Integer("Années")
     frequencyNumber = fields.Integer("Frequency number")
     partner_id = fields.Many2one("res.partner")
+    code_type_freq = fields.Selection(_get_type_freq, string="Frequency")
+
