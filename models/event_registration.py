42ccd3fe58bfd3700bcfdb6d57d135f08ee94ada
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-19 11:43:08 +0100

    Relier les participant avec la table res.partner

--- models/event_registration.py
+++ models/event_registration.py
@@ -3,3 +3,7 @@ from odoo import models, api, fields
 class EventRegistration(models.Model):
     _inherit = 'event.registration'
     resrvedBy = fields.Many2one('hr.employee', string="Réservé par")
+    partner_id = fields.Many2one("res.partner", string="Participant")
+    name = fields.Char(related="partner_id.name")
+    email = fields.Char(related="partner_id.email")
+    phone = fields.Char(related="partner_id.phone")
