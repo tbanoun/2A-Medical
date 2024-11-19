c43132e0226b72092197c25803563a903af0c0d1
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-19 21:50:29 +0100

    créer la fonction qui va créer les record des trimstre

--- models/quarter_frequency.py
+++ models/quarter_frequency.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from datetime import datetime
 
 class QuarterFrequency(models.Model):
     _name = "quarter.frequency"
@@ -9,4 +10,28 @@ class QuarterFrequency(models.Model):
     year = fields.Integer("Années")
     frequencyNumber = fields.Integer("Frequency number")
     partner_id = fields.Many2one("res.partner")
+
+
+
+    def _createRecordQuarter(self, partner_id):
+        year = datetime.now().year()
+        records = self.env['quarter.frequency'].sudo().search([
+            ('partner_id', '=', partner_id),
+            ('year', '=', int(year))
+        ])
+        if records: return True
+        for index in range(1, 5):
+            vals = {
+                "name": f"Trimestre {index}",
+                "year": int(year),
+                "frequencyNumber": 0,
+                "partner_id": partner_id
+
+
+            }
+            self.env['quarter.frequency'].sudo().create(vals)
+        return True
+
+
+
 
