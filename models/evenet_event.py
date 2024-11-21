fe0a6ec8b8d8c2069633665c941134e3e9ad35de
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-21 13:20:50 +0100

    Ajouter un champ Many2one Gamme produit sur le module événement.

--- models/evenet_event.py
+++ models/evenet_event.py
@@ -7,4 +7,6 @@ class EventEvent(models.Model):
     famille = fields.Many2one('event.tag.category', string="Famille")
     budget = fields.Monetary(string="Budget", currency_field='company_currency_id')
     company_currency_id = fields.Many2one(related='company_id.currency_id')
+    gamme_id = fields.Many2one('product.gamme', string="Gamme de Produits")
+
 
