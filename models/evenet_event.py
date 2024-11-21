7d080bd67c635a557232ce7e9ff46cf6ea7b14f7
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-21 12:05:20 +0100

    ajouter un champs bugdet sur la form view event

--- models/evenet_event.py
+++ models/evenet_event.py
@@ -5,3 +5,6 @@ class EventEvent(models.Model):
 
 
     famille = fields.Many2one('event.tag.category', string="Famille")
+    budget = fields.Monetary(string="Budget", currency_field='company_currency_id')
+    company_currency_id = fields.Many2one(related='company_id.currency_id')
+
