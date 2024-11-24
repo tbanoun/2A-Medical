0783ca79453d0318549678452ff2001388e6adc6
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-24 18:03:46 +0100

    update filed famille with categorie contact

--- models/evenet_event.py
+++ models/evenet_event.py
@@ -4,7 +4,7 @@ class EventEvent(models.Model):
     _inherit = "event.event"
 
 
-    famille = fields.Many2one('event.tag.category', string="Famille")
+    famille = fields.Many2one('customer.category', string="Catégorie")
     budget = fields.Monetary(string="Budget", currency_field='company_currency_id')
     company_currency_id = fields.Many2one(related='company_id.currency_id')
     gamme_id = fields.Many2one('product.gamme', string="Gamme de Produits")
